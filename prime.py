80e1f491e8efcfd531f910dcab65a67c4abcd976
Sean Takahashi <seanyuto@gmail.com>
2025-03-08 08:51:09 -0500

    added assignment python file

---
+++ prime.py
@@ -0,0 +1,19 @@
+## Lab01- Prime ##
+
+# You will be writing two functions, isPrime and nthPrime. 
+# Read the function documentation carefully. 
+# Test your code thoroughly.
+
+# return true if and only if x is prime, false otherwise
+# assume n is an integer
+def isPrime(n):
+
+
+
+
+
+# Return the nth prime
+# e.g. nthPrime(0) returns 2, nthPrime(1) returns 3
+def nthPrime(n):
+    # Loop through values and check if they are prime until you find n of them
+    # Use your isPrime function to check if a number is prime
